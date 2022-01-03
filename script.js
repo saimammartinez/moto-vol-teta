781f9563e1352c85730620f77745af6ba6e2cc91
saimammartinez <miky_martinez@hotmail.com>
2022-01-03 09:28:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "3 Jul 2021 06:00";
+const newYears = "2 Jul 2022 06:00";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
